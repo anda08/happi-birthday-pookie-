79b88d722d80821fbbf9cd912cb7138b5d198c8a
anda08 <andxkub16@gmail.com>
2025-07-26 12:43:37 +0200

    Add files via upload

---
+++ birthday_dream/js/wish.js
@@ -0,0 +1,29 @@
+const blowSound = document.getElementById('blowSound');
+
+function blowCandle() {
+  const candle = document.querySelector('.flame');
+  const blowSound = document.getElementById('blowSound');
+
+  if (!candle.classList.contains('blown')) {
+    candle.classList.add('blown'); // ปิดไฟเทียน
+    blowSound.play();
+
+    // รอเสียงเล่นจบหรือรอเวลาสัก 2 วิ แล้วเปลี่ยนหน้า
+    setTimeout(() => {
+      window.location.href = 'birthday.html'; // เปลี่ยนเป็นหน้าที่ต้องการไปต่อ
+    }, 2000); // 2 วินาที
+  }
+}
+
+// เอฟเฟกต์หัวใจลอยเวลาคลิก
+document.addEventListener('click', function (e) {
+  const heart = document.createElement('div');
+  heart.className = 'heart';
+  heart.style.left = e.clientX + 'px';
+  heart.style.top = e.clientY + 'px';
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 1000);
+});
